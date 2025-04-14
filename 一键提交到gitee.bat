7e1b5e4d93465537ba81bd95ab5252aee51f28f5
wuyuanwuhui99 <275018723@qq.com>
2025-04-14 21:29:57 +0800

    修改阅读文件

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,4 +1,4 @@
 git remote rm origin
-git remote add origin https://gitee.com/wuyuanwuhui99/spring-ai-service.git
+git remote add origin https://gitee.com/wuyuanwuhui99/spring-ai-service
 git push origin main
 pause
