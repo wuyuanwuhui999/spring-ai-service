09739d544bc422d27b8453a63e76b131bc855fd7
wuyuanwuhui99 <275018723@qq.com>
2025-05-08 20:34:00 +0800

    修改脚本

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,3 +1,4 @@
-git remote set-url origin https://github.com/wuyuanwuhui999/spring-ai-service
-git push origin main
-pause   
+git remote rm origin
+git remote add origin https://gitee.com/wuyuanwuhui99/spring-ai-service
+git push --set-upstream origin master
+pause
--- 一键提交到github.bat
+++ 一键提交到github.bat
@@ -1,4 +1,4 @@
-git remote set-url origin https://github.com/wuyuanwuhui999/spring-ai-service
-git push origin main
+git remote rm origin
+git remote add origin https://github.com/wuyuanwuhui999/spring-ai-service
+git push --set-upstream origin master
 pause
-
